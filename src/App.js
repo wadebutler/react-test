d3286ea6907d2cd725f1f94347539395781b7232
Wade Butler <wadebutler2@gmail.com>
2019-11-28 11:04:32 -0500

    bug fix

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import './App.css';
 class App extends Component {
   render(){
     return(
-      <h1>hello!</h1>
+      <div>
+        <h1>hello!</h1>
 
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium dolor iure, nostrum quibusdam, quod fugiat recusandae culpa esse rem nesciunt et obcaecati facere sapiente tenetur.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium dolor iure, nostrum quibusdam, quod fugiat recusandae culpa esse rem nesciunt et obcaecati facere sapiente tenetur.</p>
+      </div>
     )
   }
 }
